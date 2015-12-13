e481abbd091ac46127aba9c96714f0800c0a6213
evandvlad <evandvlad@gmail.com>
2015-12-13 18:58:40 +0300

    adjust env

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/**
+ * Autor: Evstigneev Andrey
+ * Date: 12.11.2015
+ * Time: 21:29
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    istanbul = require('gulp-istanbul'),
+    codacy = require('gulp-codacy'),
+
+    PATH_TO_SRC = './birch.js',
+    PATH_TO_TESTS = './test/**.js',
+    PATH_TO_COVERAGE_FOLDER = './coverage',
+    PATH_TO_LCOV_FILE = PATH_TO_COVERAGE_FOLDER + '/lcov.info',
+    CODACY_TOKEN = 'faf6c47899e54729bb472b9ae7c729f7';
+
+gulp.task('test.instrument', function(){
+    return gulp.src(PATH_TO_SRC)
+        .pipe(istanbul({
+            includeUntested: true
+        }))
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['test.instrument'], function(){
+    return gulp.src(PATH_TO_TESTS, {read : false})
+        .pipe(mocha())
+        .pipe(istanbul.writeReports({dir: PATH_TO_COVERAGE_FOLDER}));
+});
+
+gulp.task('codacy', function(){
+    return gulp.src(PATH_TO_LCOV_FILE)
+        .pipe(codacy({token : CODACY_TOKEN}));
+});
+
+gulp.task('deploy', ['test']);
+
+gulp.task('after-deploy', ['codacy']);
